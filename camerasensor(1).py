518f930d272cbd18965128860c92d83badb3b43c
cho-aeonian <bagchoyeong802@gmail.com>
2024-04-15 23:14:16 +0900

    0415

--- camerasensor(1).py
+++ camerasensor(1).py
@@ -16,3 +16,19 @@ objp[:,:2] = np.mgrid[0:nCols,0:nRows].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray,(nCols,nRows),None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw add display the corners
+        cv.drawChessboardCorners(img, (nCols,nRows), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
