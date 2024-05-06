de420a15c3e591889a68d755db4c635a42a843f6
cho-aeonian <bagchoyeong802@gmail.com>
2024-05-06 14:47:25 +0900

    출력 및 결과 저장

--- Histogram/histogram.py
+++ Histogram/histogram.py
@@ -35,3 +35,16 @@ res4 = cv2.LUT(img1, invol1_lut)
 hist3 = cv2.calcHist([res2], ch1, None, histSize1, ranges1)
 hist4 = cv2.calcHist([res3], ch1, None, histSize1, ranges1)
 hist5 = cv2.calcHist([res4], ch1, None, histSize1, ranges1)
+
+# 히스토그램 출력 및 결과 저장
+bin_x = np.arange(256)
+fig_index = 0
+
+ress = []
+ress.append(img1)
+ress.append(res1)
+ress.append(res2)
+ress.append(res3)
+ress.append(res4)
+
+titles = ["Input Histogram", "Equaliation-conver Histogram", "Multiply-convert Histogram", "log-convert Histogram", "Invil-convert Histgram"]
