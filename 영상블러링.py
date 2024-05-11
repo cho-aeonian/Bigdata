cec1c8a5b658bada2846ae3b5a97641fbe057f68
cho-aeonian <bagchoyeong802@gmail.com>
2024-05-11 15:36:16 +0900

    필터 정의

--- 영상블러링.py
+++ 영상블러링.py
@@ -4,3 +4,12 @@ from matplotlib import pyplot as plt
 
 # 영상 읽기
 img1 = cv2.imread("C:\Bigdata(class)\img8\img8.jpg", cv2.IMREAD_GRAYSCALE)
+
+# 필터 정의 및 블러링 수행
+ksize1 = 3; ksize2 = 5; ksize3 = 7; ksize4 = 9
+kernel = np.full(shape=[ksize4, ksize4], fill_value=1, dtype=np.float32) / (ksize4*ksize4)
+res1 = cv2.blur(img1, (ksize1, ksize1))
+res2 = cv2.blur(img1, (ksize2, ksize2))
+res3 = cv2.boxFilter(img1, -1, (ksize3, ksize3))
+res4 = cv2.filter2D(img1, -1, kernel)
+res5 = cv2.boxFilter(img1, -1, (1,21))
