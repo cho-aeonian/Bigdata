b449bb22e5313a676a8aa736e0cae6b4573d82b2
cho-aeonian <bagchoyeong802@gmail.com>
2024-05-15 22:05:02 +0900

    필터정의

--- 영상블러링(2).py
+++ 영상블러링(2).py
@@ -10,3 +10,9 @@ ksize1 = 7; ksize2 =9
 res1 = cv2.GaussianBlur(img1, (ksize1,ksize1),0)
 res2 = cv2.GaussianBlur(img1, (ksize2,ksize2),0)
 res3 = cv2.GaussianBlur(img1, (1,21),0)
+
+ress = []
+ress.append(img1), ress.append(res1),
+ress.append(res2), ress.append(res3)
+
+titles = ['input','7*7','9*9','1*21']
