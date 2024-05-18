681a095071bc8a8efc675ab7f0d9a8bf34ac339b
cho-aeonian <bagchoyeong802@gmail.com>
2024-05-19 00:43:08 +0900

    샤프닝수헹

--- 영상샤프닝.py
+++ 영상샤프닝.py
@@ -4,3 +4,7 @@ from matplotlib import pyplot as plt
 
 # 영상 밝기
 img1 = cv2.imread("C:\Bigdata(class)\img8\img8.jpg", cv2.IMREAD_GRAYSCALE)
+
+# 샤프닝 수행
+kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+res1 = cv2.filter2D(img1, -1, kernel)
