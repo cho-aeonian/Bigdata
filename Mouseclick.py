6ea8c58fee2413e8656f6e18aff529485ada7a11
cho-aeonian <bagchoyeong802@gmail.com>
2024-06-09 14:07:06 +0900

    이미지 불러오기

---
+++ Mouseclick.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# Load the image in grayscale and resize it
+img1 = cv2.imread("C:/Bigdata(class)/geometry conversion/mouse.jpg", cv2.IMREAD_GRAYSCALE)
+img1 = cv2.resize(img1, (320, 240))
