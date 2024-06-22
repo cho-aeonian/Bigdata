c0616aab1232410d795c7b97e4ff91516b793d6e
cho-aeonian <bagchoyeong802@gmail.com>
2024-06-22 09:10:53 +0900

    0622

--- birdeyeview.py
+++ birdeyeview.py
@@ -44,3 +44,9 @@ if len(points) == 4:
 
     # Apply the perspective transformation to the image
     warped = cv2.warpPerspective(img1, M, (width, height))
+
+     # Display the original image with points and the transformed image
+    plt.figure(figsize=(10, 5))
+    plt.subplot(121), plt.imshow(cv2.cvtColor(img_with_points, cv2.COLOR_BGR2RGB), cmap='gray'), plt.title('Input with Points')
+    plt.subplot(122), plt.imshow(warped, cmap='gray'), plt.title('Bird\'s Eye View')
+    plt.show()
