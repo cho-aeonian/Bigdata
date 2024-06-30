6541430d2dcd4ba72b872d5f93100a5e66a80a4d
cho-aeonian <bagchoyeong802@gmail.com>
2024-06-30 14:45:37 +0900

    0630

--- template.py
+++ template.py
@@ -26,3 +26,8 @@ for meth in methods:
     plt.figure()
     plt.subplot(121), plt.imshow(res,cmap='gray')
     plt.title('Matching Result'),plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(input, cmap='gray')
+    plt.title('Detected Point'),plt.xticks([]),plt.yticks([])
+    plt.suptitle(meth)
+
+plt.show()
