5ae1d68b60e78ce7ab13c09ee1ab78bf74b90786
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-02 14:47:13 +0900

    0702

--- corner.py
+++ corner.py
@@ -4,3 +4,8 @@ from matplotlib import pyplot as plt
 
 img1_src = cv2.imread("C:\Bigdata(class)\corner\img_6_4.png",cv2.IMREAD_GRAYSCALE)
 img1 = cv2.resize(img1_src, (320,240))
+
+dst = cv2.cornerHarris(img1,2,3,0.06)
+dst = cv2.dilate(dst,None)
+res1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
+res1[dst>0.1*dst.max()] = [0,0,255]
