1aaf2a6fadd77cae54a4e7e644e1144836afbaab
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-10 10:19:57 +0900

    0710

--- trackbar(2).py
+++ trackbar(2).py
@@ -16,3 +16,11 @@ def set_run(pos):
     itr = cv2.getTrackbarPos('iter',"morphology")
     ksize = cv2.getTrackbarPos('ksize', "morphology")
     run = cv2.getTrackbarPos('run',"morphology")
+    if run == 1:
+        ksize = check_odd(ksize)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RE, (ksize, ksize))
+        if method == 0:
+            res = cv2.erode(img1, kernel, iterations=itr)
+        else:
+            res = cv2.dilate(img1, kernel, iterations=itr)
+        cv2.imshow("morphology",res)
