46c462f49b70646ed8d7d58afee98cc83050967d
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-14 02:43:27 +0900

    0714

--- openandclose.py
+++ openandclose.py
@@ -10,3 +10,8 @@ methods = [cv2.MORPH_OPEN,
            cv2.MORPH_TOPHAT,
            cv2.MORPH_BLACKHAT,
            cv2.MORPH_HITMISS]
+ress = []
+for method in methods:
+    res = cv2.morphologyEx(img1,method, cv2.UMat(), iterations=1)
+
+    ress.append(res)
