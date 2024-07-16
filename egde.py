d08a107733667682f5ea544f0df4f9430a3273cf
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-17 01:53:01 +0900

    0718

--- egde.py
+++ egde.py
@@ -7,3 +7,5 @@ img1 = cv2.imread("C:\Bigdata(class)\edge\img16.jpg", cv2.IMREAD_GRAYSCALE)
 res = []
 img1_blur = cv2.GaussianBlur(img1, (3,3),0)
 res1 = cv2.Sobel(img1, cv2.FILTER_SCHARR, 1, 0, ksize=3)
+res2 = cv2.Scharr(img1_blur, cv2.CV_32FC1, 0, 1)
+res3 = cv2.Laplacian(img1_blur, cv2.CV_32FC1)
