c9ec8d2734ecfc7a7d5479f4efe828f7ae94cd56
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-18 01:54:24 +0900

    0718

--- egde.py
+++ egde.py
@@ -9,3 +9,7 @@ img1_blur = cv2.GaussianBlur(img1, (3,3),0)
 res1 = cv2.Sobel(img1, cv2.FILTER_SCHARR, 1, 0, ksize=3)
 res2 = cv2.Scharr(img1_blur, cv2.CV_32FC1, 0, 1)
 res3 = cv2.Laplacian(img1_blur, cv2.CV_32FC1)
+
+res4 = cv2.Canny(img1, 50, 200, apertureSize=5, L2gradient=True)
+
+res.append(img1_blur), res.append(res1), res.append(res2), res.append(res3), res.append(res4)
