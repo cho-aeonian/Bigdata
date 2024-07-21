fec5f317015cabbece1ae6d0ebb4a22e22161189
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-22 01:57:12 +0900

    0722

--- hough.py
+++ hough.py
@@ -1,3 +1,6 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+
+img1_src= cv2.imread("C:\Bigdata(class)\hough transform\img_6_0.png", cv2.IMREAD_GRAYSCALE)
+img2_src = cv2.imread("C:\Bigdata(class)\hough transform\img_6_3.png",cv2.IMREAD_GRAYSCALE)
