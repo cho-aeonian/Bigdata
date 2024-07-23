ade1a5952ff01ae57d2a9ae24f13a0a893e336fb
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-23 23:27:32 +0900

    0723

--- hough.py
+++ hough.py
@@ -4,3 +4,6 @@ from matplotlib import pyplot as plt
 
 img1_src= cv2.imread("C:\Bigdata(class)\hough transform\img_6_0.png", cv2.IMREAD_GRAYSCALE)
 img2_src = cv2.imread("C:\Bigdata(class)\hough transform\img_6_3.png",cv2.IMREAD_GRAYSCALE)
+
+img1 = cv2.resize(img1_src,(320,240))
+img2 = cv2.resize(img2_src, (320,240))
