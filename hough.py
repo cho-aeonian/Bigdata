3f375057957d9b37e87a9f5aee148df2c3133813
cho-aeonian <bagchoyeong802@gmail.com>
2024-07-24 23:28:07 +0900

    0724

--- hough.py
+++ hough.py
@@ -7,3 +7,7 @@ img2_src = cv2.imread("C:\Bigdata(class)\hough transform\img_6_3.png",cv2.IMREAD
 
 img1 = cv2.resize(img1_src,(320,240))
 img2 = cv2.resize(img2_src, (320,240))
+
+img1_edge = cv2.Canny(img1, 50, 150, apertureSize=3)
+lines = cv2.HoughLines(img1_edge, 2, np.pi/180,100)
+linesP = cv2.HoughLines(img1_edge, 2, np.pi/180, 50, minLineLength=1, maxLineGap=100)
